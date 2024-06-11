efe3c70576b43aff52438b21ca4615ffb48e28c3
Teakz9 <ybreton27@gmail.com>
2024-06-11 17:37:08 +0200

    Contenu de la page d'accueil complet et responsive

---
+++ src/componants/Footer.jsx
@@ -0,0 +1,15 @@
+import "../styles/Footer.scss";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <img
+        src="/src/images/Logo.png"
+        alt="Logo du site Trouve ton artisan coloré de différentes teintes de bleu."
+        className="footerImg"
+      />
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,8 +1,54 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../componants/Header";
+import Footer from "../componants/Footer";
+import datas from "../datas.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faStar,
+  faStarHalfAlt,
+  faStar as farStar,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/Accueil.scss";
 
 function Accueil() {
+  const [topProfiles, setTopProfiles] = useState([]);
+
+  useEffect(() => {
+    const filteredProfiles = datas.filter((profile) => profile.top === true);
+    setTopProfiles(filteredProfiles);
+  }, []);
+
+  const renderStars = (note) => {
+    const stars = [];
+    const fullStars = Math.floor(note);
+    const hasHalfStar = note % 1 !== 0;
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= fullStars) {
+        stars.push(
+          <FontAwesomeIcon key={i} icon={faStar} className="starsIcon" />
+        );
+      } else if (i === fullStars + 1 && hasHalfStar) {
+        stars.push(
+          <FontAwesomeIcon key={i} icon={faStarHalfAlt} className="starsIcon" />
+        );
+      } else {
+        stars.push(
+          <FontAwesomeIcon key={i} icon={farStar} className="starsIcon" />
+        );
+      }
+    }
+    return stars;
+  };
+
+  const navigate = useNavigate();
+
+  const handleViewDetails = (id) => {
+    navigate(`/ficheArtisans/${id}`);
+  };
+
   return (
     <div className="Accueil">
       <Header />
@@ -11,13 +57,13 @@ function Accueil() {
           Comment trouver <br />
           mon artisan ?
         </h1>
-        <span className="smallBorder"></span>
+        <span className="smallBorder1"></span>
 
-        <h2 className="stepNumber">Étape 1 :</h2>
+        <h3 className="stepNumber">Étape 1 :</h3>
         <section className="step">
-          <h3 className="stepTitle">
+          <h4 className="stepTitle">
             Choisir la catégorie d&apos;artisanat dans le menu :
-          </h3>
+          </h4>
           <img
             src="/src/images/step1Phone.png"
             alt="Capture d'écran d'une image illustrant l'étape d'un fonctionnement de site internet"
@@ -35,9 +81,9 @@ function Accueil() {
           />
         </section>
 
-        <h2 className="stepNumber">Étape 2 :</h2>
+        <h3 className="stepNumber">Étape 2 :</h3>
         <section className="step">
-          <h3 className="stepTitle">Choisir un artisan :</h3>
+          <h4 className="stepTitle">Choisir un artisan :</h4>
           <img
             src="/src/images/step2Phone.png"
             alt="Capture d'écran d'une image illustrant l'étape d'un fonctionnement de site internet"
@@ -55,11 +101,11 @@ function Accueil() {
           />
         </section>
 
-        <h2 className="stepNumber">Étape 3 :</h2>
+        <h3 className="stepNumber">Étape 3 :</h3>
         <section className="step">
-          <h3 className="stepTitle">
+          <h4 className="stepTitle">
             Le contacter via le formulaire de contact :
-          </h3>
+          </h4>
           <img
             src="/src/images/step3Phone.png"
             alt="Capture d'écran d'une image illustrant l'étape d'un fonctionnement de site internet"
@@ -77,9 +123,9 @@ function Accueil() {
           />
         </section>
 
-        <h2 className="stepNumber">Étape 4 :</h2>
+        <h3 className="stepNumber">Étape 4 :</h3>
         <section className="step">
-          <h3 className="stepTitle">Une réponse sera apportée sous 48h:</h3>
+          <h4 className="stepTitle">Une réponse sera apportée sous 48h:</h4>
           <img
             src="/src/images/step4Phone.png"
             alt="Capture d'écran d'une image illustrant l'étape d'un fonctionnement de site internet"
@@ -96,7 +142,52 @@ function Accueil() {
             className="stepDesktop stepDesktop4"
           />
         </section>
+
+        <section className="monthWorker">
+          <h2>
+            Nos 3 artisans du <br /> mois :
+          </h2>
+          <span className="smallBorder2"></span>
+
+          <section className="container">
+            <div className="row">
+              {topProfiles.map((profile) => (
+                <div
+                  className="card col-sm-1 col-md-2 col-xl-3"
+                  style={{ width: "18rem" }}
+                  key={profile.id}
+                >
+                  <div className="card-body">
+                    <img
+                      src="/src/images/avatar.png"
+                      alt="Petite illustration ovale d'un avatar"
+                      className="card-img-top avatarIcon"
+                    />
+                    <h5 className="card-title">{profile.specialty}</h5>
+                    <p className="card-text">{profile.name}</p>
+                    <div className="card-text">{renderStars(profile.note)}</div>
+                    <span className="locationBlock">
+                      <img
+                        src="/src/images/bluePing.png"
+                        alt="Petit pin bleu pour illustrer la location"
+                        className="card-img bluePing"
+                      />
+                      <p className="card-text location">{profile.location}</p>
+                    </span>
+                    <a
+                      className="btn more"
+                      onClick={() => handleViewDetails(profile.id)}
+                    >
+                      Voir plus
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
+        </section>
       </main>
+      <Footer />
     </div>
   );
 }
