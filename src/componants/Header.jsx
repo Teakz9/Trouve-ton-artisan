3204701eace5df59fc2002997f93cf8df92f5606
Teakz9 <ybreton27@gmail.com>
2024-06-17 16:50:44 +0200

    Le code JS du composant Header est commenté

--- src/componants/Header.jsx
+++ src/componants/Header.jsx
@@ -7,31 +7,36 @@ import searchIcon from "../images/searchIcon.png";
 import "../styles/Header.scss";
 
 function Header() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const navigate = useNavigate();
-  const [showSearchBar, setShowSearchBar] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [searchTerm, setSearchTerm] = useState(""); // Stocke le terme de recherche
+  const navigate = useNavigate(); // Constante pour changer la route de l'application
+  const [showSearchBar, setShowSearchBar] = useState(false); // Affiche ou masque la barre de recherche
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth); // Stocke la largeur de la fenêtre
 
+  // Constante pour le changement de la barre de recherche
   const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+    setSearchTerm(event.target.value); // Met à jour le terme de recherche selon la valeur saisie
   };
 
+  // Constante pour la soumission du formulaire de recherche
   const handleSearchSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); // Empêche le rechargement de la page
     if (searchTerm) {
-      navigate(`/listeArtisans?search=${encodeURIComponent(searchTerm)}`);
+      navigate(`/listeArtisans?search=${encodeURIComponent(searchTerm)}`); // Permet d'accéder au bon domaine d'activité
     }
   };
 
+  // Constante pour l'affichage de la barre de recherche
   const toggleSearchBar = () => {
-    setShowSearchBar(!showSearchBar);
+    setShowSearchBar(!showSearchBar); // Chznge l'état de la barre de recherche
   };
 
+  // Effet pour gérer la redimension de la fenêtre
   useEffect(() => {
     const handleResize = () => {
-      setWindowWidth(window.innerWidth);
+      setWindowWidth(window.innerWidth); // Met à jour la largeur de la fenêtre
       if (window.innerWidth >= 992) {
-        setShowSearchBar(false);
+        // Si la largeur de la fenêtre est supérieur à 992 pixels
+        setShowSearchBar(false); // Masque la barre de recherche
       }
     };
 
